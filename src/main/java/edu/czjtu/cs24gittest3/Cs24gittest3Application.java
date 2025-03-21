a2ead546dc897ef344ea61cc1e544055118db6be
tian.jinkun <tjk1898@gmail.com>
2025-03-21 09:20:28 +0800

    初始化一个SpringBoot项目

---
+++ src/main/java/edu/czjtu/cs24gittest3/Cs24gittest3Application.java
@@ -0,0 +1,13 @@
+package edu.czjtu.cs24gittest3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs24gittest3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs24gittest3Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/czjtu/cs24gittest3/Cs24gittest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.czjtu.cs24gittest3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs24gittest3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
